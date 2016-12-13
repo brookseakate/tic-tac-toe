d3d4218eda3fde6e40eb348d2ce9f310b4425552
Shari Meggs <smeggs1@gmail.com>
2016-12-13 14:25:32 -0800

    Wrote out the won functionality and have three passing tests

--- spec/board.spec.js
+++ spec/board.spec.js
@@ -6,5 +6,15 @@ describe('Board', function(){
       var board = new Board();
       expect(board.won()).toEqual(false);
     });
+    it('will return false if there is a tie', function(){
+      var board = new Board();
+      board.grid = [[1, 0, 1], [0, 0, 1], [0, 1, 0]];
+      expect(board.won()).toEqual(false);
+    });
+    it('will return true if a row or diag has the same player', function(){
+      var board = new Board();
+      board.grid = [[1, 1, 1,], [0, 0, null], [null, null, null]];
+      expect(board.won()).toEqual(true);
+    });
   });
 });
--- src/board.js
+++ src/board.js
@@ -1,9 +1,21 @@
 //Code goes up here please remember construtor
 var Board = function (){
-  this.board = [[null, null, null], [null, null, null], [null, null, null]];
+  this.grid = [[undefined, undefined, undefined], [undefined, undefined, undefined], [undefined, undefined, undefined]];
 };
 
 Board.prototype.won = function(){
-  return false;
+  var grid = this.grid;
+  for(var i = 0; i < 3; i++){
+    var vertical = grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i] &&  grid[1][i] !== undefined;
+    var horizontal = grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]  &&  grid[1][i] !== undefined;
+    var diagRight = grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]  &&  grid[1][i] !== undefined;
+    var diagLeft = grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0]  &&  grid[1][i] !== undefined;
+    if (vertical || horizontal || diagRight || diagLeft){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 };
 export default Board;
