70b19898e3c0712fc77985d2482c1042114cf5c5
Shari Meggs <smeggs1@gmail.com>
2016-12-13 13:42:17 -0800

    Wrote very basi tests for board

--- spec/board.spec.js
+++ spec/board.spec.js
@@ -1,1 +1,10 @@
 import Board from 'board';
+
+describe('Board', function(){
+  describe('won', function(){
+    it('will return false if no one has played', function(){
+      var board = new Board();
+      expect(board.won()).toEqual(false);
+    });
+  });
+});
--- src/board.js
+++ src/board.js
@@ -1,2 +1,9 @@
 //Code goes up here please remember construtor
+var Board = function (){
+  this.board = [[null, null, null], [null, null, null], [null, null, null]];
+};
+
+Board.prototype.won = function(){
+  return false;
+};
 export default Board;
