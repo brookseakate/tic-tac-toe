ed156ff0b81f13dccb8582bfe4ec93b3c93a7101
Shari Meggs <smeggs1@gmail.com>
2016-12-13 14:47:31 -0800

    created more tests for winner and captured who won the game

--- spec/board.spec.js
+++ spec/board.spec.js
@@ -11,10 +11,37 @@ describe('Board', function(){
       board.grid = [[1, 0, 1], [0, 0, 1], [0, 1, 0]];
       expect(board.won()).toEqual(false);
     });
-    it('will return true if a row or diag has the same player', function(){
+    it('will return true if a row  is the same', function(){
       var board = new Board();
-      board.grid = [[1, 1, 1,], [0, 0, null], [null, null, null]];
+      board.grid = [[1, 1, 1,], [0, 0, undefined], [undefined, undefined, undefined]];
       expect(board.won()).toEqual(true);
+    });
+    it('will return true if a diagnol is the same', function(){
+      var board = new Board();
+      board.grid = [[undefined, undefined, 0], [undefined, 0, 1], [0, 1, 0]];
+      expect(board.won()).toEqual(true);
+    });
+    it('will return true if a column is the same', function(){
+      var board = new Board();
+      board.grid = [[1, undefined, 0], [1, 0, 1], [1, 1, 0]];
+      expect(board.won()).toEqual(true);
+    });
+    it('will return false if there is a tie', function(){
+      var board = new Board();
+      board.grid = [[1, 0, 1], [undefined, undefined, undefined], [undefined, undefined, undefined]];
+      expect(board.won()).toEqual(false);
+    });
+    it('will have this winner to be 1', function(){
+      var board = new Board();
+      board.grid = [[1, undefined, 0], [1, 0, 1], [1, 1, 0]];
+      board.won();
+      expect(board.winner).toEqual(1);
+    });
+    it('will return 0 as the winner', function(){
+      var board = new Board();
+      board.grid = [[undefined, undefined, 0], [undefined, 0, 1], [0, 1, 0]];
+      board.won();
+      expect(board.winner).toEqual(0);
     });
   });
 });
--- src/board.js
+++ src/board.js
@@ -1,16 +1,30 @@
 //Code goes up here please remember construtor
 var Board = function (){
   this.grid = [[undefined, undefined, undefined], [undefined, undefined, undefined], [undefined, undefined, undefined]];
+  this.winner = undefined;
 };
 
 Board.prototype.won = function(){
   var grid = this.grid;
+  var diagRight = grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]  &&  grid[1][1] !== undefined;
+  var diagLeft = grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0]  &&  grid[1][1] !== undefined;
+  if (diagLeft){
+    this.winner = grid[0][2];
+    return true;
+  }
+  else if (diagRight) {
+    this.winner = grid[0][0];
+    return true;
+  }
   for(var i = 0; i < 3; i++){
     var vertical = grid[0][i] === grid[1][i] && grid[1][i] === grid[2][i] &&  grid[1][i] !== undefined;
-    var horizontal = grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]  &&  grid[1][i] !== undefined;
-    var diagRight = grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]  &&  grid[1][i] !== undefined;
-    var diagLeft = grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0]  &&  grid[1][i] !== undefined;
-    if (vertical || horizontal || diagRight || diagLeft){
+    var horizontal = grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]  &&  grid[i][1] !== undefined;
+    if (vertical){
+      this.winner = grid[0][i];
+      return true;
+    }
+    else if (horizontal) {
+      this.winner = grid[i][0];
       return true;
     }
     else{
