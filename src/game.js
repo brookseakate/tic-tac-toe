c1b198d7400689e6c3da39db9c33f67fe8f55a38
Shari Meggs <smeggs1@gmail.com>
2016-12-14 11:17:47 -0800

    We added the turn logic and have it tested, also added whose turn and update turn

--- src/game.js
+++ src/game.js
@@ -1,24 +1,44 @@
 import Board from 'board';
+import Player from 'player';
 
 // var board = neoard();
 
 var Game = function() {
   this.board = new Board();
+  this.playerOne = new Player(0);
+  this.playerOne.myTurn = true;
+  this.playerTwo = new Player(1);
 };
 
 // posIndex will be a string position index, '00' - '22'
 // .play returns true if play was valid; false if play was invalid
-Game.prototype.play = function(player, posIndex) {
-  if (this.board.grid[posIndex[0]][posIndex[1]] === undefined) {
-    this.board.grid[posIndex[0]][posIndex[1]] = player;
+Game.prototype.play = function(playerID, posIndex) {
+  var currentPlayer = this.whoseTurn();
+
+  if ((this.board.grid[posIndex[0]][posIndex[1]] === undefined) && (currentPlayer.idNum == playerID)) {
+    this.board.grid[posIndex[0]][posIndex[1]] = playerID;
     if (this.board.won()){
       //Call a function to end the game
     }
+    this.updateTurn();
     return true;
   } else {
     return false;
   }
-  // @TODO - add check for .won -- if won, do win sequence
+};
+Game.prototype.updateTurn = function(){
+this.playerOne.myTurn = !this.playerOne.myTurn;
+this.playerTwo.myTurn = !this.playerTwo.myTurn;
+// this.whoseTurn();
+};
+
+Game.prototype.whoseTurn = function (){
+if(this.playerOne.myTurn === true){
+  return this.playerOne;
+}
+else {
+  return this.playerTwo;
+}
 };
 
 
---
+++ src/player.js
@@ -0,0 +1,6 @@
+
+var Player = function(idNum){
+  this.myTurn = false;
+  this.idNum = idNum;
+};
+export default Player;
